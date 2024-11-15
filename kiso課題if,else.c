80b7f3850b3afd1cdb35cde86ee1050f101dbe9e
kawashimarikuto <t236129@st.yamagata-u.ac.jp>
2024-11-15 19:07:12 +0900

    Add files via upload

--- kiso課題if,else.c
+++ kiso課題if,else.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
-
 int main()
 {
-	int k;
-	printf("How old are you?");
-	scanf("%d", &k);
-	
-	if(k >=20)
-	{
-	printf("You are %d years old.  You are a fine adult!", k);
-	}
-	
-	else
-	{
-	    printf("You are still a child!");
-	}
-	return 0;
+    int k;
+    printf("How old are you?");
+    scanf("%d",&k);
+    if (k>20)
+    {
+        printf("You are a fine adult!");
+    
+}
+else if(k<20)
+{
+    printf("You are still a child!！");
+    
+}
+else (k=20);
+{
+    printf("Congratulations on your coming of age!");
+}
+    return 0;
 }
